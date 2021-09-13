56b070b9bbac6b03b50368c14311f896c3f1a80b
ccuts996 <90644291+ccuts996@users.noreply.github.com>
2021-09-13 19:30:39 -0400

    Initial commit

---
+++ src/main/java/ex05.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class ex05 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String firstnumber;
+        System.out.print(" Enter a number:");
+
+        firstnumber = input.next();
+
+        Integer i = Integer.parseInt(firstnumber);
+
+        String secondnumber;
+        System.out.print(" Enter a second number:");
+
+        secondnumber = input.next();
+
+        Integer j = Integer.parseInt(secondnumber);
+
+        int sum = j + i;
+        int difference = i - j;
+        int multiplication = i * j;
+        int division = i / j ;
+
+        System.out.print(i + " + " + j + " = " + sum + System.lineSeparator() );
+        System.out.print(i + " - " + j + " = " + difference + System.lineSeparator());
+        System.out.print(i + " * " + j + " = " + multiplication + System.lineSeparator());
+        System.out.print(i + " / " + j + " = " + division + System.lineSeparator());
+
+    }
+}
+
+
